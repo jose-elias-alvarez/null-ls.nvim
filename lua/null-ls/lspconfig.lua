61dd6fda26d7f3daba450eb699e6115ef8ce4bcf
Folke Lemaitre <folke.lemaitre@gmail.com>
2021-07-16 21:08:31 +0200

    fix: also trigger on FileType changes

--- lua/null-ls/lspconfig.lua
+++ lua/null-ls/lspconfig.lua
@@ -24,7 +24,7 @@ function M.setup()
     vim.cmd([[
       augroup null-ls
         autocmd!
-        autocmd BufReadPost * lua require("null-ls.lspconfig").try_add()
+        autocmd BufReadPost,FileType * lua require("null-ls.lspconfig").try_add()
       augroup end
     ]])
 end
