f8e2f9dcc171363ddf3bb024d040c9cc6864fb79
Jose Alvarez <j.alvarez11@icloud.com>
2021-08-13 15:37:52 +0900

    feat: add (very rough) NullLsInfo command

--- lua/null-ls/init.lua
+++ lua/null-ls/init.lua
@@ -1,9 +1,10 @@
 local methods = require("null-ls.methods")
 local helpers = require("null-ls.helpers")
 local builtins = require("null-ls.builtins")
-local rpc = require("null-ls.rpc")
 local lspconfig = require("null-ls.lspconfig")
 local handlers = require("null-ls.handlers")
+local rpc = require("null-ls.rpc")
+local u = require("null-ls.utils")
 local c = require("null-ls.config")
 
 local M = {}
@@ -29,6 +30,8 @@ M.config = function(user_config)
     rpc.setup()
     lspconfig.setup()
     handlers.setup()
+
+    vim.cmd("command! NullLsInfo lua require('null-ls').null_ls_info()")
 end
 
 -- here for backwards compatibility, but deprecated
@@ -40,6 +43,25 @@ M.setup = function(user_config)
     user_config = user_config or {}
     M.config(user_config)
     require("lspconfig")["null-ls"].setup({ on_attach = user_config.on_attach })
+end
+
+M.null_ls_info = function()
+    local ft = vim.bo.filetype
+    local registered_methods = c.get()._methods
+
+    local active = {}
+    for method, source in pairs(registered_methods) do
+        for name, filetypes in pairs(source) do
+            if u.filetype_matches(filetypes, ft) then
+                active[method] = active[method] or {}
+                table.insert(active[method], name)
+            end
+        end
+    end
+
+    -- TODO: make this nice
+    print("active sources for filetype " .. ft)
+    print(vim.inspect(active))
 end
 
 return M
