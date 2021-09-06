9dc46675b7b26fd82ec9ec9e7213ea11e1c67068
Gilles Roustan <gilles.roustan@mediapart.fr>
2021-09-06 13:56:26 +0200

    feat: introduce phpstan php static analysis tool

--- lua/null-ls/builtins/diagnostics.lua
+++ lua/null-ls/builtins/diagnostics.lua
@@ -436,4 +436,29 @@ M.codespell = h.make_builtin({
     factory = h.generator_factory,
 })
 
+M.phpstan = h.make_builtin({
+    method = DIAGNOSTICS,
+    filetypes = { "php" },
+    generator_opts = {
+        command = "phpstan",
+        args = { "analyze", "--error-format", "json", "--no-progress", "$FILENAME" },
+        format = "json_raw",
+        to_temp_file = true,
+        check_exit_code = function(code)
+            return code <= 1
+        end,
+        on_output = function(params)
+            local parser = h.diagnostics.from_json({})
+            params.messages = params.output
+                    and params.output.files
+                    and params.output.files[params.temp_path]
+                    and params.output.files[params.temp_path].messages
+                or {}
+
+            return parser({ output = params.messages })
+        end,
+    },
+    factory = h.generator_factory,
+})
+
 return M
