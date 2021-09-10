e6a20dbae6f3b10f5c3a54e8b77e5906dbd29e4c
gbprod <contact@gb-prod.fr>
2021-09-10 09:35:23 +0200

    fix diagnostics not reporting if multiple sources on the same file

--- lua/null-ls/helpers.lua
+++ lua/null-ls/helpers.lua
@@ -157,6 +157,7 @@ M.generator_factory = function(opts)
 
     return {
         fn = function(params, done)
+            params = vim.deepcopy(params)
             local loop = require("null-ls.loop")
 
             if not _validated then
