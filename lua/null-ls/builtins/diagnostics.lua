d3a25d4a6f571c24b8c89978bc47665b3440cfdb
Subho Banerjee <ssbanerje@gmail.com>
2021-10-05 15:09:14 -0500

    feat(builtins): adding proselint to builtin diagnostics

--- lua/null-ls/builtins/diagnostics.lua
+++ lua/null-ls/builtins/diagnostics.lua
@@ -93,6 +93,40 @@ M.write_good = h.make_builtin({
     factory = h.generator_factory,
 })
 
+M.proselint = h.make_builtin({
+    method = DIAGNOSTICS,
+    filetypes = { "markdown", "tex" },
+    generator_opts = {
+        command = "proselint",
+        args = { "--json" },
+        format = "json",
+        to_stdin = true,
+        check_exit_code = function(c)
+            return c <= 1
+        end,
+        on_output = function(params)
+            local diags = {}
+            local sev = {
+                error = 1,
+                warning = 2,
+                suggestion = 4,
+            }
+            for _, d in ipairs(params.output.data.errors) do
+                table.insert(diags, {
+                    row = d.line,
+                    col = d.column,
+                    end_col = d.column + d.extent - 1,
+                    code = d.check,
+                    message = d.message,
+                    severity = sev[d.severity],
+                })
+            end
+            return diags
+        end,
+    },
+    factory = h.generator_factory,
+})
+
 M.markdownlint = h.make_builtin({
     method = DIAGNOSTICS,
     filetypes = { "markdown" },
