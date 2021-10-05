c62b46b75c91e621e5c1615a6eb26c1adfd56250
Subho Banerjee <ssbanerje@gmail.com>
2021-10-05 18:43:50 -0500

    feat(builtins): adding proselint to builtin code_actions

--- lua/null-ls/builtins/code-actions.lua
+++ lua/null-ls/builtins/code-actions.lua
@@ -37,4 +37,38 @@ M.gitsigns = h.make_builtin({
     },
 })
 
+M.proselint = h.make_builtin({
+    name = "proselint",
+    method = CODE_ACTION,
+    filetypes = { "markdown", "tex" },
+    generator_opts = {
+        command = "proselint",
+        args = { "--json" },
+        format = "json",
+        to_stdin = true,
+        check_exit_code = function(c)
+            return c <= 1
+        end,
+        on_output = function(params)
+            local actions = {}
+            for _, d in ipairs(params.output.data.errors) do
+                if d.replacements ~= vim.NIL then
+                    table.insert(actions, {
+                        title = string.format("[%d] %s", d.line, d.message),
+                        action = function()
+                            local line = d.line - 1
+                            local col_beg = d.column - 1
+                            local col_end = d.column + d.extent - 2
+                            local cbuf = vim.api.nvim_get_current_buf()
+                            vim.api.nvim_buf_set_text(cbuf, line, col_beg, line, col_end, { d.replacements })
+                        end,
+                    })
+                end
+            end
+            return actions
+        end,
+    },
+    factory = h.generator_factory,
+})
+
 return M
