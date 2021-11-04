50eb36d722725f4328da1770dedc240247f645ec
Furkan Uzumcu <furkanuzumcu@gmail.com>
2021-11-03 21:40:43 -0400

    Add a spell completion source

--- lua/null-ls/builtins/completion.lua
+++ lua/null-ls/builtins/completion.lua
@@ -33,4 +33,29 @@ M.tags = h.make_builtin({
     },
 })
 
+M.spell = h.make_builtin({
+    method = COMPLETION,
+    filetypes = {},
+    name = "spell",
+    runtime_condition = function(_)
+        return vim.opt_local.spell:get()
+    end,
+    generator = {
+        fn = function(params, done)
+            local candidates = function(entries)
+                local items = {}
+                for k, v in ipairs(entries) do
+                    items[k] = { label = v, kind = vim.lsp.protocol.CompletionItemKind["Text"] }
+                end
+
+                return items
+            end
+
+            done(candidates(vim.fn.spellsuggest(params.word_to_complete)))
+        end,
+        async = true,
+        use_cache = true,
+    },
+})
+
 return M
