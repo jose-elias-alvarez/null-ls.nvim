3165cd60710f3478e8bf9845e5d5936a6e0f212e
Furkan Uzumcu <furkanuzumcu@gmail.com>
2021-11-04 18:46:35 -0400

    Add completion tags source back
    Looks like I was doing the wrong thing before and that's why this was
    slow. It should be fine now.

--- lua/null-ls/builtins/completion.lua
+++ lua/null-ls/builtins/completion.lua
@@ -1,10 +1,45 @@
 local h = require("null-ls.helpers")
 local methods = require("null-ls.methods")
-local utils = require("null-ls.utils")
 
 local COMPLETION = methods.internal.COMPLETION
 
 local M = {}
+
+M.tags = h.make_builtin({
+    method = COMPLETION,
+    filetypes = {},
+    name = "tags",
+    generator_opts = {
+        runtime_condition = function(_)
+            return #vim.fn.tagfiles() > 0
+        end,
+    },
+    generator = {
+        fn = function(params, done)
+            local tags = vim.fn.taglist(params.word_to_complete)
+            if tags == 0 then
+                done({ items = {}, isIncomplete = false })
+                return
+            end
+
+            local words = {}
+            local items = {}
+            for _, tag in ipairs(tags) do
+                table.insert(words, tag.name)
+            end
+            words = table.uniq(words)
+            for _, word in ipairs(words) do
+                table.insert(items, {
+                    label = word,
+                    insertText = word,
+                })
+            end
+
+            done({ items = items, isIncomplete = false })
+        end,
+        async = true,
+    },
+})
 
 M.spell = h.make_builtin({
     method = COMPLETION,
