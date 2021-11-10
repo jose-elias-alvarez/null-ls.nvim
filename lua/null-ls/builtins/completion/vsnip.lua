c124f0ab5de22d21cc5940d92d79750e817c2263
oncomouse <oncomouse@gmail.com>
2021-11-10 08:48:18 -0600

    Stylua formatting

--- lua/null-ls/builtins/completion/vsnip.lua
+++ lua/null-ls/builtins/completion/vsnip.lua
@@ -4,26 +4,25 @@ local methods = require("null-ls.methods")
 local COMPLETION = methods.internal.COMPLETION
 
 return h.make_builtin({
-	method = COMPLETION,
-	filetypes = {},
-	name = "vsnip",
-	generator = {
-		fn = function(params, done)
-			local items = {}
-			local snips = vim.fn["vsnip#get_complete_items"](params.bufnr)
-			local targets = vim.tbl_filter(function(item)
-				return string.match(item.word, "^" .. params.word_to_complete)
-			end, snips)
-			for _, item in ipairs(targets) do
-				table.insert(items, {
-					label = item.abbr,
-					detail = item.menu,
-					kind = vim.lsp.protocol.CompletionItemKind["Snippet"],
-				})
-			end
-			done({ { items = items, isIncomplete = false } })
-		end,
-		async = true,
-	},
+    method = COMPLETION,
+    filetypes = {},
+    name = "vsnip",
+    generator = {
+        fn = function(params, done)
+            local items = {}
+            local snips = vim.fn["vsnip#get_complete_items"](params.bufnr)
+            local targets = vim.tbl_filter(function(item)
+                return string.match(item.word, "^" .. params.word_to_complete)
+            end, snips)
+            for _, item in ipairs(targets) do
+                table.insert(items, {
+                    label = item.abbr,
+                    detail = item.menu,
+                    kind = vim.lsp.protocol.CompletionItemKind["Snippet"],
+                })
+            end
+            done({ { items = items, isIncomplete = false } })
+        end,
+        async = true,
+    },
 })
-
