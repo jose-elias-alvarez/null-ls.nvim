5b3cd5767ad66085b14167775ceb8ac42b0fcc61
kylo252 <59826753+kylo252@users.noreply.github.com>
2021-12-09 21:12:11 +0100

    ci: remove the auto-commit part since it's broken

---
+++ lua/null-ls/builtins/_meta/completion.lua
@@ -0,0 +1,13 @@
+-- THIS FILE IS GENERATED. DO NOT EDIT MANUALLY.
+-- stylua: ignore
+return {
+  spell = {
+    filetypes = {}
+  },
+  tags = {
+    filetypes = {}
+  },
+  vsnip = {
+    filetypes = {}
+  }
+}
