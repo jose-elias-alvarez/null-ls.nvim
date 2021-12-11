f38a805f001feaa13151059beeb75ae245980c72
oncomouse <oncomouse@gmail.com>
2021-12-11 10:49:35 -0600

    Display the expanded snippet in the documentation field.

--- lua/null-ls/builtins/completion/vsnip.lua
+++ lua/null-ls/builtins/completion/vsnip.lua
@@ -11,14 +11,27 @@ return h.make_builtin({
         fn = function(params, done)
             local items = {}
             local snips = vim.fn["vsnip#get_complete_items"](params.bufnr)
-            local targets = vim.tbl_filter(function(item)
-                return string.match(item.word, "^" .. params.word_to_complete)
-            end, snips)
-            for _, item in ipairs(targets) do
+            for _, item in ipairs(snips) do
+                local user_data = vim.fn.json_decode(item.user_data)
+
+                -- Extract snippet description (adapted from cmp-vsnip):
+                local documentation = {}
+                table.insert(documentation, string.format("```%s", params.ft))
+                for _, line in ipairs(vim.split(vim.fn["vsnip#to_string"](user_data.vsnip.snippet), "\n")) do
+                    table.insert(documentation, line)
+                end
+                table.insert(documentation, "```")
+				documentation = vim.lsp.util.convert_input_to_markdown_lines(documentation)
+				documentation = table.concat(documentation, "\n")
+
                 table.insert(items, {
                     label = item.abbr,
                     detail = item.menu,
-                    kind = vim.lsp.protocol.CompletionItemKind["Snippet"],
+                    kind = vim.lsp.protocol.CompletionItemKind.Snippet,
+                    documentation = {
+                        value = documentation,
+                        kind = vim.lsp.protocol.MarkupKind.Markdown,
+                    },
                 })
             end
             done({ { items = items, isIncomplete = #items == 0 } })
