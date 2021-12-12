8ac30ae3741cfca155fecf8c0fc20b45d524b3bf
Ettienne Pitts <ettpitts@pm.me>
2021-12-12 20:37:07 +0200

    fix: formatting

--- lua/null-ls/builtins/diagnostics/cspell.lua
+++ lua/null-ls/builtins/diagnostics/cspell.lua
@@ -3,28 +3,26 @@ local methods = require("null-ls.methods")
 
 local DIAGNOSTICS = methods.internal.DIAGNOSTICS
 
-return h.make_builtin(
-  {
+return h.make_builtin({
     method = DIAGNOSTICS,
     filetypes = {},
     generator_opts = {
-      command = "cspell",
-      args = {"stdin"},
-      to_stdin = true,
-      ignore_stderr = true,
-      format = "line",
-      check_exit_code = function(code)
-        return code <= 1
-      end,
-      on_output = h.diagnostics.from_pattern(
-        [[.*:(%d+):(%d+)%s*-%s*(.*%((.*)%))]],
-        {"row", "col", "message", "_quote"},
-        {
-          adapters = {h.diagnostics.adapters.end_col.from_quote},
-          offsets = {end_col = 1}
-        }
-      )
+        command = "cspell",
+        args = { "stdin" },
+        to_stdin = true,
+        ignore_stderr = true,
+        format = "line",
+        check_exit_code = function(code)
+            return code <= 1
+        end,
+        on_output = h.diagnostics.from_pattern(
+            [[.*:(%d+):(%d+)%s*-%s*(.*%((.*)%))]],
+            { "row", "col", "message", "_quote" },
+            {
+              adapters = { h.diagnostics.adapters.end_col.from_quote },
+              offsets = { end_col = 1 }
+            }
+        )
     },
-    factory = h.generator_factory
-  }
-)
+    factory = h.generator_factory,
+})
