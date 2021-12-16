2c808fb8a722907f98a2817c2e86c908a25aaadb
kylo252 <59826753+kylo252@users.noreply.github.com>
2021-12-16 19:13:56 +0100

    ci: add plenary to the runtimepath

--- scripts/autogen_metadata.sh
+++ scripts/autogen_metadata.sh
@@ -5,7 +5,8 @@ declare -x XDG_DATA_HOME="${XDG_DATA_HOME:-"$HOME/.local/share"}"
 declare -x NVIM_PACK_DIR="$XDG_DATA_HOME/nvim/site/pack"
 
 declare -x NULL_LS_DIR="$PWD"
+declare -x PLENARY_DIR="$NVIM_PACK_DIR/packer/start/plenary.nvim"
 
 nvim -u NONE --headless \
-  --cmd "set rtp=$VIMRUNTIME,${NULL_LS_DIR}" \
+  --cmd "set rtp=${VIMRUNTIME},${NULL_LS_DIR},${PLENARY_DIR}" \
   +"luafile scripts/autogen_metadata.lua" +q
