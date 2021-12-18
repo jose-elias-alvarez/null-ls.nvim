e0482e6775637969fca8e00a07a2159321f144b9
BarrOff <58253563+BarrOff@users.noreply.github.com>
2021-12-18 13:39:43 +0100

    add ptop Pascal code formatter to builtins

---
+++ lua/null-ls/builtins/formatting/ptop.lua
@@ -0,0 +1,15 @@
+local h = require("null-ls.helpers")
+local methods = require("null-ls.methods")
+
+local FORMATTING = methods.internal.FORMATTING
+
+return h.make_builtin({
+    method = FORMATTING,
+    filetypes = { "pascal", "delphi" },
+    generator_opts = {
+        command = "ptop",
+        args = { "$FILENAME", "$FILENAME" },
+        to_temp_file = true,
+    },
+    factory = h.formatter_factory,
+})
