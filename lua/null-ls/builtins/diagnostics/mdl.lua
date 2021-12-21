dfe8eee9cb81afcc0470688c6937bb4a6ff65a98
Dennis B <bluz71@users.noreply.github.com>
2021-12-21 15:18:17 +1100

    Convert tabs to spaces
    Command-line 'stylua' emits tabs, yet the null-ls repo appears to want
    spaces.
    
    Hopefully this is more conforming.

--- lua/null-ls/builtins/diagnostics/mdl.lua
+++ lua/null-ls/builtins/diagnostics/mdl.lua
@@ -4,26 +4,26 @@ local methods = require("null-ls.methods")
 local DIAGNOSTICS = methods.internal.DIAGNOSTICS
 
 return h.make_builtin({
-	method = DIAGNOSTICS,
-	filetypes = { "markdown" },
-	generator_opts = {
-		command = "mdl",
-		args = { "--json" },
-		to_stdin = true,
-		format = "json",
-		check_exit_code = function(c)
-			return c <= 1
-		end,
-		on_output = h.diagnostics.from_json({
-			attributes = {
-				row = "line",
-				code = "rule",
-				message = "description",
-			},
-			diagnostic = {
-				severity = h.diagnostics.severities.warning,
-			},
-		}),
-	},
-	factory = h.generator_factory,
+    method = DIAGNOSTICS,
+    filetypes = { "markdown" },
+    generator_opts = {
+        command = "mdl",
+        args = { "--json" },
+        to_stdin = true,
+        format = "json",
+        check_exit_code = function(c)
+            return c <= 1
+        end,
+        on_output = h.diagnostics.from_json({
+            attributes = {
+                row = "line",
+                code = "rule",
+                message = "description",
+            },
+            diagnostic = {
+                severity = h.diagnostics.severities.warning,
+            },
+        }),
+    },
+    factory = h.generator_factory,
 })
--- lua/null-ls/builtins/diagnostics/standardjs.lua
+++ lua/null-ls/builtins/diagnostics/standardjs.lua
@@ -4,22 +4,22 @@ local methods = require("null-ls.methods")
 local DIAGNOSTICS = methods.internal.DIAGNOSTICS
 
 return h.make_builtin({
-	method = DIAGNOSTICS,
-	filetypes = { "javascript", "javascriptreact" },
-	generator_opts = {
-		command = "standard",
-		args = { "--stdin" },
-		to_stdin = true,
-		ignore_stderr = true,
-		format = "line",
-		check_exit_code = function(c)
-			return c <= 1
-		end,
-		on_output = h.diagnostics.from_pattern(":(%d+):(%d+): (.*)", { "row", "col", "message" }, {
-			diagnostic = {
-				severity = h.diagnostics.severities.error,
-			},
-		}),
-	},
-	factory = h.generator_factory,
+    method = DIAGNOSTICS,
+    filetypes = { "javascript", "javascriptreact" },
+    generator_opts = {
+        command = "standard",
+        args = { "--stdin" },
+        to_stdin = true,
+        ignore_stderr = true,
+        format = "line",
+        check_exit_code = function(c)
+            return c <= 1
+        end,
+        on_output = h.diagnostics.from_pattern(":(%d+):(%d+): (.*)", { "row", "col", "message" }, {
+            diagnostic = {
+                severity = h.diagnostics.severities.error,
+            },
+        }),
+    },
+    factory = h.generator_factory,
 })
