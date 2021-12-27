997b4bef4a42e5731a86a94d914ff8225e8af531
Kevin Greene <kgreenek@gmail.com>
2021-12-26 20:24:02 -1000

    Add comment for -path arg in buildifier formatting builtin

--- lua/null-ls/builtins/formatting/buildifier.lua
+++ lua/null-ls/builtins/formatting/buildifier.lua
@@ -8,6 +8,8 @@ return h.make_builtin({
     filetypes = { "bzl" },
     generator_opts = {
         command = "buildifier",
+        -- This is needed for buildifier to be able to deduce the relative workspace path.
+        -- Without this, sorting sources and deps doesn't appear to work.
         args = { "-path=$FILENAME" },
         to_stdin = true,
     },
