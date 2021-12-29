95d454308d43e065a62228c931c739904bae2e75
Seth Daniel <seth@sethdaniel.org>
2021-12-29 10:54:55 -0600

    feat(builtin): add editorconfig-checker

---
+++ lua/null-ls/builtins/diagnostics/editorconfig_checker.lua
@@ -0,0 +1,21 @@
+local h = require("null-ls.helpers")
+local methods = require("null-ls.methods")
+
+local DIAGNOSTICS = methods.internal.DIAGNOSTICS
+
+return h.make_builtin({
+    method = DIAGNOSTICS,
+    filetypes = {},
+    generator_opts = {
+        command = "ec",
+        args = {
+            "-no-color",
+            "$FILENAME",
+        },
+        to_stdin = true,
+        from_stderr = true,
+        format = "line",
+        on_output = h.diagnostics.from_pattern([[(%d+): (.+)]], { "row", "message" }),
+    },
+    factory = h.generator_factory,
+})
