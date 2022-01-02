04332e21763a3216c7ac70fdb57576963a9faeaf
Tynan McAuley <16469394+tymcauley@users.noreply.github.com>
2022-01-01 23:32:23 -0800

    feat(builtin): Add `gitlint` diagnostic

---
+++ lua/null-ls/builtins/diagnostics/gitlint.lua
@@ -0,0 +1,27 @@
+local h = require("null-ls.helpers")
+local methods = require("null-ls.methods")
+
+local DIAGNOSTICS = methods.internal.DIAGNOSTICS
+
+return h.make_builtin({
+    name = "gitlint",
+    method = DIAGNOSTICS,
+    filetypes = { "gitcommit" },
+    generator_opts = {
+        command = "gitlint",
+        args = { "--msg-filename", "$FILENAME" },
+        to_temp_file = true,
+        from_stderr = true,
+        format = "line",
+        check_exit_code = function(code)
+            return code <= 1
+        end,
+        on_output = h.diagnostics.from_patterns({
+            {
+                pattern = [[(%d+): (%w+) (.+)]],
+                groups = { "row", "code", "message" },
+            },
+        }),
+    },
+    factory = h.generator_factory,
+})
