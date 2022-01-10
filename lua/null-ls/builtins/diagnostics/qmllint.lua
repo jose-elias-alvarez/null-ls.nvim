0c1eceda6cb33d5ab788c135555eb5af7d79d266
Furkan Uzumcu <furkanuzumcu@gmail.com>
2022-01-10 14:04:48 -0500

    Fix qmllint

--- lua/null-ls/builtins/diagnostics/qmllint.lua
+++ lua/null-ls/builtins/diagnostics/qmllint.lua
@@ -9,12 +9,18 @@ return h.make_builtin({
     filetypes = { "qml" },
     generator_opts = {
         command = "qmllint",
-        args = { "--no-unqualified-id", "$FILENAME" },
+        args = { "$FILENAME" },
         to_stdin = false,
         format = "raw",
         from_stderr = true,
         to_temp_file = true,
-        on_output = h.diagnostics.from_errorformat(table.concat({ "%trror: %m", "%f:%l : %m" }, ","), "qmllint"),
+        on_output = h.diagnostics.from_errorformat(
+            table.concat({
+                "%trror: %f:%l:%c: %m",
+                "%tarning: %f:%l:%c: %m",
+            }, ","),
+            "qmllint"
+        ),
     },
     factory = h.generator_factory,
 })
