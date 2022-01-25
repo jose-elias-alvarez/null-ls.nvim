20a8d943639a7b4966fe3bfeded2e8636cd9ee42
Chad Transtrum <chad.transtrum@earlywarning.com>
2022-01-24 18:46:54 -0700

    fix stylua complaints

--- lua/null-ls/builtins/formatting/standardjs.lua
+++ lua/null-ls/builtins/formatting/standardjs.lua
@@ -5,14 +5,14 @@ local methods = require("null-ls.methods")
 local FORMATTING = methods.internal.FORMATTING
 
 return h.make_builtin({
-  name = "standardjs",
-  method = FORMATTING,
-  filetypes = { "javascript", "javascriptreact" },
-  generator_opts = {
-    command = "standard",
-    args = { "--stdin", "--fix" },
-    to_stdin = true,
-    dynamic_command = cmd_resolver.from_node_modules,
-  },
-  factory = h.formatter_factory,
+    name = "standardjs",
+    method = FORMATTING,
+    filetypes = { "javascript", "javascriptreact" },
+    generator_opts = {
+        command = "standard",
+        args = { "--stdin", "--fix" },
+        to_stdin = true,
+        dynamic_command = cmd_resolver.from_node_modules,
+    },
+    factory = h.formatter_factory,
 })
