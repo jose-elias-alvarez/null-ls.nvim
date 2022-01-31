21fd7839156a4df221f83fb299173056bc42f206
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2022-01-31 09:35:17 +0100

    Add diagnostics and formatter for Puppet
    Based on the Rubocop formatter. This uses puppet-lint.
    
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

---
+++ lua/null-ls/builtins/diagnostics/puppet_lint.lua
@@ -0,0 +1,39 @@
+local h = require("null-ls.helpers")
+local methods = require("null-ls.methods")
+local u = require("null-ls.utils")
+
+local severities = {
+    error = vim.lsp.protocol.DiagnosticSeverity.Error,
+    warning = vim.lsp.protocol.DiagnosticSeverity.Warning,
+}
+
+return h.make_builtin({
+    name = "puppet-lint",
+    method = methods.internal.DIAGNOSTICS,
+    filetypes = { "puppet", "epuppet" },
+    generator_opts = {
+        command = "puppet-lint",
+        args = { "--json", "$FILENAME" },
+        format = "json",
+        check_exit_code = function(code)
+            return code <= 1
+        end,
+        on_output = function(params)
+            local diags = {}
+            for _, d in ipairs(params.output) do
+                for _, f in ipairs(d) do
+                    table.insert(diags, {
+                        row = f.line,
+                        col = f.column,
+                        source = f.check,
+                        message = f.message,
+                        severity = severities[f.kind],
+                        filename = f.fullpath,
+                    })
+                end
+            end
+            return diags
+        end,
+    },
+    factory = h.generator_factory,
+})
