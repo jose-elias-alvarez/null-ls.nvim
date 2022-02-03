bd9c2feb2b07d692893c860742c610fc2ef10790
Andrew Ferrier <andrewferrier@example.com>
2022-02-03 14:21:56 +0000

    feat(builtins): Add checkmake diagnostics

---
+++ lua/null-ls/builtins/diagnostics/checkmake.lua
@@ -0,0 +1,31 @@
+local h = require("null-ls.helpers")
+local methods = require("null-ls.methods")
+
+local DIAGNOSTICS = methods.internal.DIAGNOSTICS
+
+return h.make_builtin({
+    name = "checkmake",
+    method = DIAGNOSTICS,
+    filetypes = { "make" },
+    generator_opts = {
+        command = "checkmake",
+        args = {
+            "--format='{{.LineNumber}}:{{.Rule}}:{{.Violation}}'",
+            "$FILENAME",
+        },
+        to_stdin = false,
+        from_stderr = false,
+        to_temp_file = true,
+        format = "line",
+        check_exit_code = function(code)
+            return code >= 1
+        end,
+        on_output = h.diagnostics.from_patterns({
+            {
+                pattern = [[(%d+):(%w+):(.+)]],
+                groups = { "row", "code", "message" },
+            },
+        }),
+    },
+    factory = h.generator_factory,
+})
