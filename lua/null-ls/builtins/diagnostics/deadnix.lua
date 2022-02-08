0ade7d9f165d702a693f7aec151c9f8e7f6af21c
ilkecan <ilkecan@protonmail.com>
2022-02-08 19:51:47 +0300

    feat(builtins): add deadnix diagnostics

---
+++ lua/null-ls/builtins/diagnostics/deadnix.lua
@@ -0,0 +1,29 @@
+local h = require("null-ls.helpers")
+local methods = require("null-ls.methods")
+
+local DIAGNOSTICS = methods.internal.DIAGNOSTICS
+
+return h.make_builtin({
+    name = "deadnix",
+    method = DIAGNOSTICS,
+    filetypes = { "nix" },
+    generator_opts = {
+        command = "deadnix",
+        args = { "--output-format=json", "$FILENAME" },
+        to_temp_file = true,
+        format = "json",
+        on_output = function(params)
+            params.output = params.output and params.output.results or {}
+            local parser = h.diagnostics.from_json({
+                attributes = {
+                    source = "deadnix",
+                },
+                diagnostic = {
+                    severity = h.diagnostics.severities.warning,
+                },
+            })
+            return parser(params)
+        end,
+    },
+    factory = h.generator_factory,
+})
