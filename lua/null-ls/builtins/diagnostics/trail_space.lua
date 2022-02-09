b2896eadafaf5034e2fed8964b170fb07cb5a86a
Andrew Ferrier <andrewferrier@example.com>
2022-02-09 18:16:58 +0000

    feat: Add trailing space diagnostic

---
+++ lua/null-ls/builtins/diagnostics/trail_space.lua
@@ -0,0 +1,37 @@
+local h = require("null-ls.helpers")
+local methods = require("null-ls.methods")
+
+local DIAGNOSTICS = methods.internal.DIAGNOSTICS
+local SEVERITIES = h.diagnostics.severities
+
+return h.make_builtin({
+    name = "trailing-space",
+    method = DIAGNOSTICS,
+    filetypes = {},
+    generator = {
+        async = true,
+        fn = function(params, done)
+            local result = {}
+            local regex = vim.regex("\\s\\+$")
+
+            for line_number = 1, #params.content do
+                local line = params.content[line_number]
+                local start_byte, end_byte = regex:match_str(line)
+
+                if start_byte ~= nil then
+                    table.insert(result, {
+                        message = "trailing whitespace",
+                        severity = SEVERITIES.warning,
+                        row = line_number,
+                        col = start_byte + 1,
+                        end_row = line_number,
+                        end_col = end_byte + 1,
+                        source = "trail_space",
+                    })
+                end
+            end
+
+            return done(result)
+        end,
+    },
+})
