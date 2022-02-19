867b6e226a8fff97d7752dffabcea9fdb60e30ef
Andreas Schneider <asn@cryptomilk.org>
2022-02-19 13:58:18 +0100

    feat(builtins): Add rpmspec diagnostics builtin

---
+++ lua/null-ls/builtins/diagnostics/rpmspec.lua
@@ -0,0 +1,35 @@
+local h = require("null-ls.helpers")
+local methods = require("null-ls.methods")
+
+local DIAGNOSTICS = methods.internal.DIAGNOSTICS
+
+return h.make_builtin({
+    name = "rpmspec",
+    method = DIAGNOSTICS,
+    filetypes = { "spec" },
+    generator_opts = {
+        command = "rpmspec",
+        args = {
+            "-P",
+            "$FILENAME",
+        },
+        to_stdin = false,
+        from_stderr = true,
+        to_temp_file = true,
+        format = "line",
+        check_exit_code = { 0, 1 },
+        on_output = h.diagnostics.from_patterns({
+            -- error
+            {
+                pattern = [[(%w+): (.*): line (%d+): (.*)]],
+                groups = { "severity", "filename", "row", "message" },
+            },
+            -- warning
+            {
+                pattern = [[(%w+): (.*) in line (%d+):]],
+                groups = { "severity", "message", "row" },
+            },
+        }),
+    },
+    factory = h.generator_factory,
+})
