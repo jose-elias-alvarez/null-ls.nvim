b0c27471204b1faed5d7700a6912eacb602119d8
mvllow <mvllow@icloud.com>
2022-02-24 13:29:01 -0500

    add filetypes

--- lua/null-ls/builtins/code_actions/xo.lua
+++ lua/null-ls/builtins/code_actions/xo.lua
@@ -1,5 +1,6 @@
 return require("null-ls.builtins").code_actions.eslint.with({
     name = "xo",
+    filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
     command = "xo",
     args = { "--reporter", "json", "--stdin", "--stdin-filename", "$FILENAME" },
 })
--- lua/null-ls/builtins/diagnostics/xo.lua
+++ lua/null-ls/builtins/diagnostics/xo.lua
@@ -1,5 +1,6 @@
 return require("null-ls.builtins").diagnostics.eslint.with({
     name = "xo",
+    filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
     command = "xo",
     args = { "--reporter", "json", "--stdin", "--stdin-filename", "$FILENAME" },
 })
