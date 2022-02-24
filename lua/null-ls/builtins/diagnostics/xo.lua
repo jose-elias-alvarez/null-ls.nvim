09bc0e2212254e6d115aeff5c3d197ce8f9fe8bf
mvllow <mvllow@icloud.com>
2022-02-24 13:24:06 -0500

    extend eslint

--- lua/null-ls/builtins/diagnostics/xo.lua
+++ lua/null-ls/builtins/diagnostics/xo.lua
@@ -1,43 +1,5 @@
-local h = require("null-ls.helpers")
-local cmd_resolver = require("null-ls.helpers.command_resolver")
-local methods = require("null-ls.methods")
-
-local DIAGNOSTICS = methods.internal.DIAGNOSTICS
-
-local handle_xo_output = function(params)
-    params.messages = params.output and params.output[1] and params.output[1].messages or {}
-    if params.err then
-        table.insert(params.messages, { message = params.err })
-    end
-
-    local parser = h.diagnostics.from_json({
-        attributes = {
-            severity = "severity",
-        },
-        severities = {
-            h.diagnostics.severities["warning"],
-            h.diagnostics.severities["error"],
-        },
-    })
-
-    return parser({ output = params.messages })
-end
-
-return h.make_builtin({
+return require("null-ls.builtins").diagnostics.eslint.with({
     name = "xo",
-    method = DIAGNOSTICS,
-    filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
-    generator_opts = {
-        command = "xo",
-        args = { "--reporter", "json", "--stdin", "--stdin-filename", "$FILENAME" },
-        to_stdin = true,
-        format = "json",
-        check_exit_code = function(code)
-            return code <= 1
-        end,
-        use_cache = true,
-        on_output = handle_xo_output,
-        dynamic_command = cmd_resolver.from_node_modules,
-    },
-    factory = h.generator_factory,
+    command = "xo",
+    args = { "--reporter", "json", "--stdin", "--stdin-filename", "$FILENAME" },
 })
