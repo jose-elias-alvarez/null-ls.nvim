1ee1da4970b3c94bed0d0250a353bff633901cd1
Jose Alvarez <j.alvarez11@icloud.com>
2022-03-10 21:46:22 -0500

    chore: format

--- lua/null-ls/builtins/diagnostics/haml_lint.lua
+++ lua/null-ls/builtins/diagnostics/haml_lint.lua
@@ -4,40 +4,40 @@ local methods = require("null-ls.methods")
 local DIAGNOSTICS = methods.internal.DIAGNOSTICS
 
 local handle_haml_lint_output = function(params)
-  local file = params.output.files[1]
-  if params.output and params.output.files then
-    local parser = h.diagnostics.from_json({ })
-    local offenses = {}
+    local file = params.output.files[1]
+    if params.output and params.output.files then
+        local parser = h.diagnostics.from_json({})
+        local offenses = {}
 
-    for _, offense in ipairs(file.offenses) do
-      table.insert(offenses, {
-        message = offense.message,
-        line = offense.location.line,
-        ruleId = offense.linter_name,
-        level = offense.severity,
-      })
+        for _, offense in ipairs(file.offenses) do
+            table.insert(offenses, {
+                message = offense.message,
+                line = offense.location.line,
+                ruleId = offense.linter_name,
+                level = offense.severity,
+            })
+        end
+
+        return parser({ output = offenses })
     end
 
-    return parser({ output = offenses })
-  end
-
-  return {}
+    return {}
 end
 
 return h.make_builtin({
-  name = "haml-lint",
-  method = DIAGNOSTICS,
-  filetypes = { "haml" },
-  generator_opts = {
-    command = "haml-lint",
-    args = { "--reporter", "json", "$FILENAME" },
-    to_stdin = true,
-    from_stderr = true,
-    format = "json",
-    check_exit_code = function(code)
-      return code <= 1
-    end,
-    on_output = handle_haml_lint_output,
-  },
-  factory = h.generator_factory,
+    name = "haml-lint",
+    method = DIAGNOSTICS,
+    filetypes = { "haml" },
+    generator_opts = {
+        command = "haml-lint",
+        args = { "--reporter", "json", "$FILENAME" },
+        to_stdin = true,
+        from_stderr = true,
+        format = "json",
+        check_exit_code = function(code)
+            return code <= 1
+        end,
+        on_output = handle_haml_lint_output,
+    },
+    factory = h.generator_factory,
 })
