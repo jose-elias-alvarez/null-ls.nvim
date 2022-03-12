19d72762181c257fa4fe337b76e2261a25e9a8ea
Jose Alvarez <j.alvarez11@icloud.com>
2022-03-12 13:25:45 -0500

    chore: remove duplicate meta key

--- lua/null-ls/builtins/diagnostics/xo.lua
+++ lua/null-ls/builtins/diagnostics/xo.lua
@@ -7,8 +7,4 @@ return require("null-ls.builtins").diagnostics.eslint.with({
     filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
     command = "xo",
     args = { "--reporter", "json", "--stdin", "--stdin-filename", "$FILENAME" },
-    meta = {
-        url = "https://github.com/xojs/xo",
-        description = "❤️ JavaScript/TypeScript linter (ESLint wrapper) with great defaults",
-    },
 })
