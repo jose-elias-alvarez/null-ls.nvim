23e491f56a9cac98d80ff5837790e09ba70bcac7
Ahmed Hashim <ahashim@users.noreply.github.com>
2022-03-18 15:50:39 -0400

    feat: adding solhint diagnostics to builtins

---
+++ lua/null-ls/builtins/diagnostics/solhint.lua
@@ -0,0 +1,35 @@
+local h = require("null-ls.helpers")
+local DIAGNOSTICS = require("null-ls.methods").internal.DIAGNOSTICS
+
+return h.make_builtin({
+    name = "solhint",
+    meta = {
+        url = "https://protofire.github.io/solhint/",
+        description = "An open source project for linting Solidity code. It provides both security and style guide validations.",
+    },
+    method = DIAGNOSTICS,
+    filetypes = { "solidity" },
+    factory = h.generator_factory,
+    generator_opts = {
+        args = {
+            "$FILENAME",
+            "--formatter",
+            "unix",
+        },
+        command = "solhint",
+        format = "line",
+        from_stderr = false,
+        on_output = h.diagnostics.from_pattern("([^:]+):([%d]+):([%d]+): (.*) %[([%a]+)/([^]]+)%]", {
+            "filename",
+            "row",
+            "col",
+            "message",
+            "severity",
+            "code",
+        }, {
+            severities = {
+                ["Warning"] = h.diagnostics.severities.warning,
+            },
+        }),
+    },
+})
