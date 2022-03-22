f2a2f8c612234aa35652e16d98edf963f95f4f05
Ahmed Hashim <ahashim@users.noreply.github.com>
2022-03-21 21:56:28 -0400

    feat: adding solhint diagnostic support for solidity files (#774)
    * feat: adding solhint diagnostics to builtins
    
    * fix: updating line pattern to match & ensuring output gets sent from std_err
    to std_in for solhint builtin
    
    * Adding solidity documentation to BUILTINS.md
    
    * test: Adding solhint tests

---
+++ lua/null-ls/builtins/diagnostics/solhint.lua
@@ -0,0 +1,37 @@
+local h = require("null-ls.helpers")
+local DIAGNOSTICS = require("null-ls.methods").internal.DIAGNOSTICS
+
+return h.make_builtin({
+    name = "solhint",
+    meta = {
+        url = "https://protofire.github.io/solhint/",
+        description = "An open source project for linting Solidity code. It provides both security and style guide validations.",
+    },
+    method = DIAGNOSTICS,
+    filetypes = { "solidity" },
+    factory = h.generator_factory,
+    generator_opts = {
+        args = {
+            "$FILENAME",
+            "--formatter",
+            "unix",
+        },
+        command = "solhint",
+        format = "line",
+        from_stderr = true,
+        on_output = h.diagnostics.from_pattern("([^:]*):([%d]+):([%d]+): (.*) %[([%a]+)/([%a%p]+)%]", {
+            "filename",
+            "row",
+            "col",
+            "message",
+            "severity",
+            "code",
+        }, {
+            severities = {
+                ["Error"] = h.diagnostics.severities.error,
+                ["Warning"] = h.diagnostics.severities.warning,
+            },
+        }),
+        to_stdin = true,
+    },
+})
