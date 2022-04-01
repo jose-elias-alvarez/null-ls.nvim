1d46c02bef189df08749b0f8da1235786a79e42a
Serdar Sayın <25414028+ssayin@users.noreply.github.com>
2022-04-01 19:51:30 +0300

    Update verible_verilog_format.lua

--- lua/null-ls/builtins/formatting/verible_verilog_format.lua
+++ lua/null-ls/builtins/formatting/verible_verilog_format.lua
@@ -7,14 +7,14 @@ return h.make_builtin({
     name = "verible_verilog_format",
     meta = {
         url = "https://github.com/chipsalliance/verible",
-        description = "The verible-verilog-format formatter manages whitespace in accordance with a particular style. The main goal is to relieve humans of having to manually manage whitespace, wrapping, and indentation, and to provide a tool that can be integrated into any editor to enable editor-independent consistency."
+        description = "The verible-verilog-format formatter manages whitespace in accordance with a particular style. The main goal is to relieve humans of having to manually manage whitespace, wrapping, and indentation, and to provide a tool that can be integrated into any editor to enable editor-independent consistency.",
     },
     method = FORMATTING,
-    filetypes = {"verilog", "systemverilog"},
+    filetypes = { "verilog", "systemverilog" },
     generator_opts = {
         command = "verible-verilog-format",
-        args = {"--stdin_name", "$FILENAME", "-"},
-        to_stdin = true
+        args = { "--stdin_name", "$FILENAME", "-" },
+        to_stdin = true,
     },
-    factory = h.formatter_factory
+    factory = h.formatter_factory,
 })
