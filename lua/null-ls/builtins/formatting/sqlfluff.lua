1949c7b51298a5ae41cd8791d2825457e6100601
Denis Cornehl <denis@cornehl.org>
2022-04-17 07:22:33 +0200

    extend sqlfluff builtin documentation to add needed dialect

--- lua/null-ls/builtins/formatting/sqlfluff.lua
+++ lua/null-ls/builtins/formatting/sqlfluff.lua
@@ -8,6 +8,15 @@ return h.make_builtin({
     meta = {
         url = "https://github.com/sqlfluff/sqlfluff",
         description = "A SQL linter and auto-formatter for Humans",
+        notes = {
+            "SQLFluff needs a mandatory `--dialect` argument. Use `extra_args` to add yours. `extra_args` can also be a function to build more sophisticated logic.",
+        },
+        usage = [[
+local sources = {
+	null_ls.builtins.formatting.sqlfluff.with({
+		extra_args = {"--dialect", "postgres"} -- change to your dialect
+	})
+}]],
     },
     method = FORMATTING,
     filetypes = { "sql" },
