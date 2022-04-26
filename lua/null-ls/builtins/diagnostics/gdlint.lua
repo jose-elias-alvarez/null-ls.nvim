5c463c1917e2efc6c2788b76e52ed1cab409523d
Arrow-x <Houssem.kun26@gmail.com>
2022-04-26 14:05:33 +0100

    added gdlint

---
+++ lua/null-ls/builtins/diagnostics/gdlint.lua
@@ -0,0 +1,36 @@
+local h = require("null-ls.helpers")
+local methods = require("null-ls.methods")
+
+local DIAGNOSTICS = methods.internal.DIAGNOSTICS
+
+return h.make_builtin({
+    name = "gdlint",
+    meta = {
+        url = "",
+        description = "",
+    },
+    method = DIAGNOSTICS,
+    filetypes = { "gdscript" },
+    generator_opts = {
+        command = "gdlint",
+        args = { "$FILENAME" },
+        from_stderr = false,
+        -- ignore_stderr = true,
+        format = "line",
+        --      check_exit_code = function(code)
+        -- print("we are out")
+        -- return code > 0
+        --      end,
+        on_output = h.diagnostics.from_patterns({
+            {
+                pattern = [[:(%d+): (.*)]],
+                groups = { "row", "message" },
+                -- overrides = {
+                --     diagnostic = { severity = INFO },
+                --     offsets = { col = 1 },
+                -- },
+            },
+        }),
+    },
+    factory = h.generator_factory,
+})
