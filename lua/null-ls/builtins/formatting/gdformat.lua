3ecdf1ebefb2829d9e9b59e8208f1e3d3074db14
Arrow-x <Houssem.kun26@gmail.com>
2022-04-26 12:29:28 +0100

    Formatting

--- lua/null-ls/builtins/formatting/gdformat.lua
+++ lua/null-ls/builtins/formatting/gdformat.lua
@@ -5,19 +5,19 @@ local FORMATTING = methods.internal.FORMATTING
 -- local RANGE_FORMATTING = methods.internal.RANGE_FORMATTING
 
 return h.make_builtin({
-    name = "gdformat",
-    meta = {
-        url = "https://github.com/Scony/godot-gdscript-toolkit",
-        description = "A formatter for Godot's gdscript",
-    },
-    method = { FORMATTING },
-    filetypes = { "gd", "gdscript", "gdscript3" },
-    generator_opts = {
-        command = "gdformat",
+	name = "gdformat",
+	meta = {
+		url = "https://github.com/Scony/godot-gdscript-toolkit",
+		description = "A formatter for Godot's gdscript",
+	},
+	method = { FORMATTING },
+	filetypes = { "gd", "gdscript", "gdscript3" },
+	generator_opts = {
+		command = "gdformat",
 		args = {
 			"-",
 		},
-        to_stdin = true,
-    },
-    factory = h.formatter_factory,
+		to_stdin = true,
+	},
+	factory = h.formatter_factory,
 })
