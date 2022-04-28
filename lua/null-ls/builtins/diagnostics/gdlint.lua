51b6a25b5b55d7d89bec96dcb922f8603d90397e
Arrow-x <Houssem.kun26@gmail.com>
2022-04-28 22:58:26 +0100

    Added godot-gdscript-toolkit linter

--- lua/null-ls/builtins/diagnostics/gdlint.lua
+++ lua/null-ls/builtins/diagnostics/gdlint.lua
@@ -6,29 +6,25 @@ local DIAGNOSTICS = methods.internal.DIAGNOSTICS
 return h.make_builtin({
     name = "gdlint",
     meta = {
-        url = "",
-        description = "",
+        url = "https://github.com/Scony/godot-gdscript-toolkit",
+        description = "A linter that performs a static analysis on gdscript code according to some predefined configuration.",
     },
     method = DIAGNOSTICS,
     filetypes = { "gdscript" },
     generator_opts = {
         command = "gdlint",
         args = { "$FILENAME" },
+        to_stdin = false,
         from_stderr = true,
-        -- ignore_stderr = true,
+        to_temp_file = true,
         format = "line",
-        --      check_exit_code = function(code)
-        -- print("we are out")
-        -- return code > 0
-        --      end,
+        check_exit_code = function(code)
+            return code > 0
+        end,
         on_output = h.diagnostics.from_patterns({
             {
                 pattern = [[:(%d+): (.*)]],
                 groups = { "row", "message" },
-                -- overrides = {
-                --     diagnostic = { severity = INFO },
-                --     offsets = { col = 1 },
-                -- },
             },
         }),
     },
