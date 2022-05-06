1232123ed1c45a8f3980f4c82053c934d4e29a5f
Yura Shatunov <46374280+volucris1@users.noreply.github.com>
2022-05-06 22:19:30 +0700

    Fix (builtins): stylelint format from stderr

--- lua/null-ls/builtins/formatting/stylelint.lua
+++ lua/null-ls/builtins/formatting/stylelint.lua
@@ -16,6 +16,8 @@ return h.make_builtin({
         command = "stylelint",
         args = { "--fix", "--stdin", "--stdin-filename", "$FILENAME" },
         to_stdin = true,
+        from_stderr = true,
+        ignore_stderr = false,
         dynamic_command = cmd_resolver.from_node_modules,
     },
     factory = h.formatter_factory,
