17dd2c95a7412e97bf9752a5a5cca95581baf361
Dean Gaffney <deanpatrickgaffney@gmail.com>
2022-06-13 23:45:23 +0200

    feat: added default runtime condition for cfn_lint diagnostics source (#913)
    * feat: added default runtime condition for cfn_lint to only produce diagnostics for aws cloudformation files
    
    * docs: added cfn_lint notes regarding buffer cache

--- lua/null-ls/builtins/diagnostics/cfn_lint.lua
+++ lua/null-ls/builtins/diagnostics/cfn_lint.lua
@@ -31,6 +31,23 @@ return h.make_builtin({
                 },
             }
         ),
+        runtime_condition = h.cache.by_bufnr(function(params)
+            -- check if file looks like a cloudformation template
+            local lines = vim.api.nvim_buf_get_lines(params.bufnr, 0, -1, false)
+            for _, line in ipairs(lines) do
+                if line:match("Resources") or line:match("AWSTemplateFormatVersion") then
+                    return true
+                end
+            end
+            return false
+        end),
+        notes = {
+            "Once a supported file type is opened null-ls will try and determine if the file looks like an AWS Cloudformation template.",
+            'A file will be considered an AWS Cloudformation template if it contains "Resources" or "AWSTemplateFormatVersion".',
+            "This check will run only once when entering the buffer.",
+            'This means if "Resources" or "AWSTemplateFormatVersion" is added to a file after this check is run, the cfn-lint diagnostics will not be generated.',
+            "To fix this you must restart neovim.",
+        },
     },
     factory = h.generator_factory,
 })
