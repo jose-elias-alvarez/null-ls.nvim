0d68b5bffa2e15ff06b4dc42b293a0f6e3060da5
Hori Ryota <hori.ryota@gmail.com>
2022-06-26 00:08:39 +0900

    feat: define prettier cwd (#921)
    * feat: define prettier cwd
    
    * fix import

--- lua/null-ls/builtins/formatting/prettier.lua
+++ lua/null-ls/builtins/formatting/prettier.lua
@@ -1,6 +1,7 @@
 local h = require("null-ls.helpers")
 local cmd_resolver = require("null-ls.helpers.command_resolver")
 local methods = require("null-ls.methods")
+local u = require("null-ls.utils")
 
 local FORMATTING = methods.internal.FORMATTING
 local RANGE_FORMATTING = methods.internal.RANGE_FORMATTING
@@ -41,6 +42,22 @@ return h.make_builtin({
         }, "--range-start", "--range-end", { row_offset = -1, col_offset = -1 }),
         to_stdin = true,
         dynamic_command = cmd_resolver.from_node_modules,
+        cwd = h.cache.by_bufnr(function(params)
+            return u.root_pattern(
+                -- https://prettier.io/docs/en/configuration.html
+                ".prettierrc",
+                ".prettierrc.json",
+                ".prettierrc.yml",
+                ".prettierrc.yaml",
+                ".prettierrc.json5",
+                ".prettierrc.js",
+                ".prettierrc.cjs",
+                ".prettier.config.js",
+                ".prettier.config.cjs",
+                ".prettierrc.toml",
+                "package.json"
+            )(params.bufname)
+        end),
     },
     factory = h.formatter_factory,
 })
--- lua/null-ls/builtins/formatting/prettier_standard.lua
+++ lua/null-ls/builtins/formatting/prettier_standard.lua
@@ -1,6 +1,7 @@
 local h = require("null-ls.helpers")
 local cmd_resolver = require("null-ls.helpers.command_resolver")
 local methods = require("null-ls.methods")
+local u = require("null-ls.utils")
 
 local FORMATTING = methods.internal.FORMATTING
 
@@ -17,6 +18,22 @@ return h.make_builtin({
         args = { "--stdin" },
         to_stdin = true,
         dynamic_command = cmd_resolver.from_node_modules,
+        cwd = h.cache.by_bufnr(function(params)
+            return u.root_pattern(
+                -- https://prettier.io/docs/en/configuration.html
+                ".prettierrc",
+                ".prettierrc.json",
+                ".prettierrc.yml",
+                ".prettierrc.yaml",
+                ".prettierrc.json5",
+                ".prettierrc.js",
+                ".prettierrc.cjs",
+                ".prettier.config.js",
+                ".prettier.config.cjs",
+                ".prettierrc.toml",
+                "package.json"
+            )(params.bufname)
+        end),
     },
     factory = h.formatter_factory,
 })
