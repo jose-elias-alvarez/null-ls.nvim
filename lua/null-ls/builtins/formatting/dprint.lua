3b58754b6c8d43b36b1c142d8667b805e27319f8
Syd Amir <hemedani@gmail.com>
2022-08-10 11:57:05 +0430

    :art: format file and remove config path

--- lua/null-ls/builtins/formatting/dprint.lua
+++ lua/null-ls/builtins/formatting/dprint.lua
@@ -4,32 +4,30 @@ local methods = require("null-ls.methods")
 local FORMATTING = methods.internal.FORMATTING
 
 return h.make_builtin({
-	name = "dprint",
-	meta = {
-		url = "https://dprint.dev/",
-		description = "A pluggable and configurable code formatting platform written in Rust.",
-	},
-	method = FORMATTING,
-	filetypes = {
-		"javascript",
-		"javascriptreact",
-		"typescript",
-		"typescriptreact",
-		"json",
-		"markdown",
-		"toml",
-		"rust",
-		"roslyn",
-	},
-	generator_opts = {
-		command = "dprint",
-		args = {
-			"fmt",
-			"--config",
-			"https://raw.githubusercontent.com/hemedani/dotfiles/main/dprint/dprint.json",
-			"--stdin",
-		},
-		to_stdin = true,
-	},
-	factory = h.formatter_factory,
+    name = "dprint",
+    meta = {
+        url = "https://dprint.dev/",
+        description = "A pluggable and configurable code formatting platform written in Rust.",
+    },
+    method = FORMATTING,
+    filetypes = {
+        "javascript",
+        "javascriptreact",
+        "typescript",
+        "typescriptreact",
+        "json",
+        "markdown",
+        "toml",
+        "rust",
+        "roslyn",
+    },
+    generator_opts = {
+        command = "dprint",
+        args = {
+            "fmt",
+            "--stdin",
+        },
+        to_stdin = true,
+    },
+    factory = h.formatter_factory,
 })
