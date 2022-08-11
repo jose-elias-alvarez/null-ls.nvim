9f7366a76a9a406a8b7c1a8c66c0bd17cdc93b86
SydAmir <hemedani@gmail.com>
2022-08-11 18:31:44 +0430

    :wrench: add dprint to builtins formatter (#998)
    * :wrench: add dprint to builtins formatter
    
    * :art: format file and remove config path
    
    * :memo: add note for dprint init

---
+++ lua/null-ls/builtins/formatting/dprint.lua
@@ -0,0 +1,36 @@
+local h = require("null-ls.helpers")
+local methods = require("null-ls.methods")
+
+local FORMATTING = methods.internal.FORMATTING
+
+return h.make_builtin({
+    name = "dprint",
+    meta = {
+        url = "https://dprint.dev/",
+        description = "A pluggable and configurable code formatting platform written in Rust.",
+        notes = {
+            [[you need to install dprint to use this builtin and then run `dprint init` to initialize it in your project directory.]],
+        },
+    },
+    method = FORMATTING,
+    filetypes = {
+        "javascript",
+        "javascriptreact",
+        "typescript",
+        "typescriptreact",
+        "json",
+        "markdown",
+        "toml",
+        "rust",
+        "roslyn",
+    },
+    generator_opts = {
+        command = "dprint",
+        args = {
+            "fmt",
+            "--stdin",
+        },
+        to_stdin = true,
+    },
+    factory = h.formatter_factory,
+})
