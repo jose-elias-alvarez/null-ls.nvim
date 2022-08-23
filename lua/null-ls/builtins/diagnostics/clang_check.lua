ecabd930bf46537a3c869be998549692b6480ba0
Daniil Shvalov <shvalovich@gmail.com>
2022-08-23 12:43:57 +0300

    feat(diagnostics): add clang-check

---
+++ lua/null-ls/builtins/diagnostics/clang_check.lua
@@ -0,0 +1,41 @@
+local h = require("null-ls.helpers")
+local methods = require("null-ls.methods")
+
+local DIAGNOSTICS = methods.internal.DIAGNOSTICS
+
+return h.make_builtin({
+    name = "clang_check",
+    meta = {
+        url = "https://releases.llvm.org/14.0.0/tools/clang/docs/ClangTools.html",
+        description = "ClangCheck combines the LibTooling framework for running a Clang tool with the basic Clang diagnostics by syntax checking specific files in a fast, command line interface.",
+        notes = {
+            "`clang-check` will be run only when files are saved to disk, so that `compile_commands.json` files can be used. It is recommended to use this linter in combination with `compile_commands.json` files.",
+        },
+    },
+    method = DIAGNOSTICS,
+    filetypes = { "c", "cpp" },
+    generator_opts = {
+        command = "clang-check",
+        args = {
+            "--analyze",
+            "--extra-arg=-Xclang",
+            "--extra-arg=-analyzer-output=text",
+            "--extra-arg=-fno-color-diagnostics",
+            "-p",
+            "build",
+            "$FILENAME",
+        },
+        from_stderr = true,
+        format = "line",
+        check_exit_code = function(code)
+            return code <= 1
+        end,
+        on_output = h.diagnostics.from_patterns({
+            {
+                pattern = ":(%d+):(%d+): (%w+): (.*)$",
+                groups = { "row", "col", "severity", "message" },
+            },
+        }),
+    },
+    factory = h.generator_factory,
+})
