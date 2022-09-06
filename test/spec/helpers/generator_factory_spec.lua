82cae2b9cba7e28a75754a6041feda15bb8bba31
Jose Alvarez <j.alvarez11@icloud.com>
2022-09-06 13:55:44 -0400

    feat(loop): create temp files at source location

--- test/spec/helpers/generator_factory_spec.lua
+++ test/spec/helpers/generator_factory_spec.lua
@@ -1,3 +1,4 @@
+local mock = require("luassert.mock")
 local stub = require("luassert.stub")
 
 local c = require("null-ls.config")
@@ -5,12 +6,15 @@ local helpers = require("null-ls.helpers")
 local loop = require("null-ls.loop")
 local s = require("null-ls.state")
 
+mock(require("null-ls.logger"), true)
+
 local test_utils = require("null-ls.test-utils")
 local root = vim.fn.getcwd()
 
 describe("generator_factory", function()
     stub(loop, "spawn")
     stub(loop, "temp_file")
+    stub(loop, "read_file")
     stub(s, "get_cache")
     stub(s, "set_cache")
 
@@ -40,6 +44,7 @@ describe("generator_factory", function()
 
         loop.spawn:clear()
         loop.temp_file:clear()
+        loop.read_file:clear()
 
         s.get_cache:clear()
         s.set_cache:clear()
@@ -574,8 +579,8 @@ describe("generator_factory", function()
                 cleanup:clear()
             end)
 
-            it("should call loop.temp_file with content and file extension", function()
-                assert.stub(loop.temp_file).was_called_with("buffer content", "lua")
+            it("should call loop.temp_file with content and bufname", function()
+                assert.stub(loop.temp_file).was_called_with("buffer content", params.bufname)
             end)
 
             it("should replace $FILENAME arg with temp path", function()
@@ -597,20 +602,11 @@ describe("generator_factory", function()
 
         describe("from_temp_file", function()
             local cleanup = stub.new()
-            stub(vim.loop, "fs_open")
-            stub(vim.loop, "fs_fstat")
-            stub(vim.loop, "fs_read")
-            stub(vim.loop, "fs_close")
-
-            local mock_fd = 99
-            local mock_stat = { size = 100 }
 
             local params
             local on_stdout_end
             before_each(function()
                 loop.temp_file.returns("temp-path", cleanup)
-                vim.loop.fs_open.returns(99)
-                vim.loop.fs_fstat.returns(mock_stat)
 
                 generator_opts.to_temp_file = true
                 generator_opts.from_temp_file = true
@@ -621,22 +617,17 @@ describe("generator_factory", function()
                 on_stdout_end = loop.spawn.calls[1].refs[3].on_stdout_end
             end)
             after_each(function()
-                vim.loop.fs_open:clear()
-                vim.loop.fs_fstat:clear()
                 cleanup:clear()
             end)
 
-            it("should call vim.loop methods", function()
+            it("should call loop.read_file with temp path", function()
                 on_stdout_end()
 
-                assert.stub(vim.loop.fs_open).was_called_with("temp-path", "r", 438)
-                assert.stub(vim.loop.fs_fstat).was_called_with(mock_fd)
-                assert.stub(vim.loop.fs_read).was_called_with(mock_fd, mock_stat.size, 0)
-                assert.stub(vim.loop.fs_close).was_called_with(mock_fd)
+                assert.stub(loop.read_file).was_called_with("temp-path")
             end)
 
             it("should set params.output to temp file content", function()
-                vim.loop.fs_read.returns("content")
+                loop.read_file.returns("content")
 
                 on_stdout_end()
 
