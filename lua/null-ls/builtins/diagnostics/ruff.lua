e19eb90c8a58b74b9b3eb9c2c19d4852266563cb
vsedov <viv.sedov@hotmail.com>
2022-09-07 11:42:02 +0100

    refactor(ruff): Allow for severity and have a similar setup to flake8

--- lua/null-ls/builtins/diagnostics/ruff.lua
+++ lua/null-ls/builtins/diagnostics/ruff.lua
@@ -1,6 +1,50 @@
 local h = require("null-ls.helpers")
 local methods = require("null-ls.methods")
 local DIAGNOSTICS = methods.internal.DIAGNOSTICS
+local custom_end_col = {
+    end_col = function(entries, line)
+        if not line then
+            return
+        end
+
+        local start_col = entries["col"]
+        local message = entries["message"]
+        local code = entries["code"]
+        local default_position = start_col + 1
+
+        local pattern = nil
+        local trimmed_line = line:sub(start_col, -1)
+
+        if code == "F841" or code == "F823" then
+            pattern = [[Local variable %`(.*)%`]]
+        elseif code == "F821" or code == "F822" then
+            pattern = [[Undefined name %`(.*)%`]]
+        elseif code == "F401" then
+            pattern = [[%`(.*)%` imported but unused]]
+        elseif code == "F841" then
+            pattern = [[Local variable %`(.*)%` is assigned to but never used]]
+        elseif code == "R001" then
+            pattern = [[Class %`(.*)%` inherits from object]]
+        end
+        if not pattern then
+            return default_position
+        end
+
+        local results = message:match(pattern)
+        local _, end_col = trimmed_line:find(results, 1, true)
+
+        if not end_col then
+            return default_position
+        end
+
+        end_col = end_col + start_col
+        if end_col > tonumber(start_col) then
+            return end_col
+        end
+
+        return default_position
+    end,
+}
 
 return h.make_builtin({
     name = "ruff",
@@ -16,17 +60,26 @@ return h.make_builtin({
             "-n",
             "$FILENAME",
         },
-        from_stderr = true,
         format = "line",
         check_exit_code = function(code)
             return code == 0
         end,
-        on_output = h.diagnostics.from_patterns({
+        to_stdin = false,
+        from_stderr = true,
+        on_output = h.diagnostics.from_pattern(
+            [[(%d+):(%d+): ((%u)%w+) (.*)]],
+            { "row", "col", "code", "severity", "message" },
             {
-                pattern = "(.*):([0-9]+):([0-9]+): ([A-Z][0-9]+) (.*)",
-                groups = { "file", "row", "col", "type", "message" },
-            },
-        }),
+                adapters = {
+                    custom_end_col,
+                },
+                severities = {
+                    E = h.diagnostics.severities["error"],
+                    F = h.diagnostics.severities["warning"],
+                    R = h.diagnostics.severities["error"],
+                },
+            }
+        ),
     },
     factory = h.generator_factory,
 })
