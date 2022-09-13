379ea8d33d80aa95332ed068bd81e8137c507d2c
Jose Alvarez <j.alvarez11@icloud.com>
2022-09-13 14:11:28 -0400

    test: fix range formatting

--- test/spec/e2e_spec.lua
+++ test/spec/e2e_spec.lua
@@ -23,6 +23,29 @@ local get_code_actions = function()
     )
 end
 
+-- borrowed from Neovim's implementation until we can use vim.region()
+local range_formatting = function()
+    local start = vim.fn.getpos("v")
+    local end_ = vim.fn.getpos(".")
+    local start_row = start[2]
+    local start_col = start[3]
+    local end_row = end_[2]
+    local end_col = end_[3]
+
+    if start_row == end_row and end_col < start_col then
+        end_col, start_col = start_col, end_col
+    elseif end_row < start_row then
+        start_row, end_row = end_row, start_row
+        start_col, end_col = end_col, start_col
+    end
+    local range = {
+        ["start"] = { start_row, start_col - 1 },
+        ["end"] = { end_row, end_col - 1 },
+    }
+
+    lsp.buf.format({ range = range })
+end
+
 describe("e2e", function()
     after_each(function()
         vim.cmd("bufdo! bdelete!")
@@ -295,9 +318,9 @@ describe("e2e", function()
         end)
 
         it("should format specified range", function()
-            vim.cmd("normal ggV")
+            vim.cmd("silent normal ggV")
 
-            lsp.buf.range_formatting()
+            range_formatting()
             wait_for_prettier()
 
             assert.equals(u.buf.content(nil, true), formatted)
