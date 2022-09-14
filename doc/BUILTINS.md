77f7814b61cbfd235f7f9ce7d4ad74628ec3ab6c
kaiuri <uriel.acioli@gmail.com>
2022-09-14 11:32:03 -0300

    feat(commitlint): remove external dependencies from source `commitlint`
    Previous version of this source depended on `@commitlint/config-conventional`
    and `commitlint-format-json`.
    
    Signed-off-by: Uriel C. A. Fernandes <uriel.acioli@gmail.com>

--- doc/BUILTINS.md
+++ doc/BUILTINS.md
@@ -463,12 +463,15 @@ local sources = { null_ls.builtins.diagnostics.commitlint }
 - Filetypes: `{ "gitcommit" }`
 - Method: `diagnostics`
 - Command: `commitlint`
-- Args: `{ "--format", "commitlint-format-json" }`
+- Args: `{ "-g", "~/.commitlintrc.json", "--edit", "$FILENAME" }`
 
 #### Notes
 
-- Needs npm packages commitlint and a json formatter: `@commitlint/{config-conventional,cli}` and `commitlint-format-json`.
-- It works with the packages installed globally but watch out for [some common issues](https://github.com/conventional-changelog/commitlint/issues/613).
+- Current `gitcommit` buffer contents are saved to temp file, therefore `$FILENAME` is dynamically generated. 
+- Needs a `~/.commitlintrc.json` configuration file ([schema](https://json.schemastore.org/commitlintrc.json)), or overriding the path to befit your extension preferences.
+- An example of `commitlintrc.json` can be found [here](https://gist.github.com/kaiuri/f28c0aeb6c44d4604714ad451aa5b644).
+<!-- - Needs npm packages commitlint and a json formatter: `@commitlint/{config-conventional,cli}` and `commitlint-format-json`. -->
+<!-- - It works with the packages installed globally but watch out for [some common issues](https://github.com/conventional-changelog/commitlint/issues/613). -->
 
 ### [cppcheck](https://github.com/danmar/cppcheck)
 
--- lua/null-ls/builtins/diagnostics/commitlint.lua
+++ lua/null-ls/builtins/diagnostics/commitlint.lua
@@ -3,7 +3,8 @@ local methods = require("null-ls.methods")
 
 local DIAGNOSTICS = methods.internal.DIAGNOSTICS
 
-local violation_to_diagnostic = function(violation)
+-- NOTE: Unused
+--[==[ local violation_to_diagnostic = function(violation)
     local diagnostic = {
         ruleId = violation.name,
         message = violation.message,
@@ -18,8 +19,10 @@ local violation_to_diagnostic = function(violation)
 
     return diagnostic
 end
+--]==]
 
-local handle_commitlint_output = function(params)
+-- NOTE: Unused
+--[==[ local handle_commitlint_output = function(params)
     if params.output and params.output.results then
         local output = params.output.results[1]
 
@@ -42,6 +45,46 @@ local handle_commitlint_output = function(params)
         return parser({ output = violations })
     end
 end
+--]==]
+
+---@param line string
+local function get_severity(line)
+    local severities = {
+        ["✖"] = vim.diagnostic.severity.ERROR,
+        ["⚠"] = vim.diagnostic.severity.WARN,
+        -- NOTE: ↓ INFO is currently disable since it doesn't seem to produce anything but the same link
+        -- ['^ⓘ'] = vim.diagnostic.severity.INFO
+    }
+    for pattern, severity in pairs(severities) do
+        -- NOTE: Can be  used to futher improve parsing.
+        local pos = { vim.regex("^" .. pattern):match_str(line) }
+
+        return pos ~= nil and severity
+    end
+end
+
+---@param line string: line from command output
+---@param _ {}: `params`
+local function on_output(line, _)
+    local valid_line = line:match("]$") and line
+
+    if not valid_line then
+        return nil
+    else
+        local severity = get_severity(valid_line)
+        return severity
+                and {
+                    row = 1,
+                    col = 1,
+                    source = "commitlint",
+                    message = valid_line:match("%s%s%s(.*)%[.*"),
+                    code = valid_line:match("%[.*%]"),
+                    severity = severity,
+                    filename = "",
+                }
+            or nil
+    end
+end
 
 return h.make_builtin({
     name = "commitlint",
@@ -49,21 +92,25 @@ return h.make_builtin({
         url = "https://commitlint.js.org",
         description = "commitlint checks if your commit messages meet the conventional commit format.",
         notes = {
-            "Needs npm packages commitlint and a json formatter: `@commitlint/{config-conventional,cli}` and `commitlint-format-json`.",
-            "It works with the packages installed globally but watch out for [some common issues](https://github.com/conventional-changelog/commitlint/issues/613).",
+            -- "Needs npm packages commitlint and a json formatter: `@commitlint/{config-conventional,cli}` and `commitlint-format-json`.",
+            -- "It works with the packages installed globally but watch out for [some common issues](https://github.com/conventional-changelog/commitlint/issues/613).",
+            [=[Needs a `commitlintrc.json` configuration file, [schema](https://json.schemastore.org/commitlintrc.json).]=],
         },
     },
     method = DIAGNOSTICS,
     filetypes = { "gitcommit" },
     generator_opts = {
         command = "commitlint",
-        args = { "--format", "commitlint-format-json" },
-        to_stdin = true,
-        format = "json",
+        -- args = { "--format", "commitlint-format-json" },
+        -- NOTE: Maybe add a function for resolving the configuration path?
+        args = { "-g", vim.fs.normalize("~/.commitlintrc.json"), "--edit", "$FILENAME" },
+        -- to_stdin = true,
+        to_temp_file = true,
+        format = "line",
         check_exit_code = function(code)
             return code <= 1
         end,
-        on_output = handle_commitlint_output,
+        on_output = on_output,
     },
     factory = h.generator_factory,
 })
