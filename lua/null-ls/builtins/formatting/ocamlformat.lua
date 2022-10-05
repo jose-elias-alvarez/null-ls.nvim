cf62fb5c9bb3b2861a248c9ad4a1cf427c7bd180
c3n21 <me@zhifan.me>
2022-10-05 21:27:13 +0200

    add(ocamlformat): support for ocamlformat

---
+++ lua/null-ls/builtins/formatting/ocamlformat.lua
@@ -0,0 +1,20 @@
+local h = require("null-ls.helpers")
+local methods = require("null-ls.methods")
+
+local FORMATTING = methods.internal.FORMATTING
+
+return h.make_builtin({
+    name = "ocamlformat",
+    meta = {
+        url = "https://github.com/ocaml-ppx/ocamlformat",
+        description = "Auto-formatter for OCaml code",
+    },
+    method = FORMATTING,
+    filetypes = { "ocaml" },
+    generator_opts = {
+        command = "ocamlformat",
+        args = { "--enable-outside-detected-project", "-" },
+        to_stdin = true,
+    },
+    factory = h.formatter_factory,
+})
