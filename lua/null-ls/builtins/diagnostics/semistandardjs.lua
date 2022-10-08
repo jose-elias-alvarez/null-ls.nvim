ea537458b406588706271ca6790c4a92d58104e7
Johan Rivera <rivera.johan23@gmail.com>
2022-10-08 15:21:47 -0400

    feat: add builtin config for semistandarsjs (#1171)

---
+++ lua/null-ls/builtins/diagnostics/semistandardjs.lua
@@ -0,0 +1,47 @@
+local h = require("null-ls.helpers")
+local cmd_resolver = require("null-ls.helpers.command_resolver")
+local methods = require("null-ls.methods")
+
+local DIAGNOSTICS = methods.internal.DIAGNOSTICS
+
+return h.make_builtin({
+    name = "semistandardjs",
+    meta = {
+        url = "https://github.com/standard/semistandard",
+        description = "JavaScript style guide, linter, and formatter.",
+    },
+    method = DIAGNOSTICS,
+    filetypes = { "javascript", "javascriptreact" },
+    generator_opts = {
+        command = "semistandard",
+        args = { "--stdin" },
+        to_stdin = true,
+        ignore_stderr = true,
+        format = "line",
+        check_exit_code = function(c)
+            return c <= 1
+        end,
+        on_output = h.diagnostics.from_patterns({
+            {
+                pattern = ":(%d+):(%d+): Parsing error: (.*)",
+                groups = { "row", "col", "message" },
+                overrides = {
+                    diagnostic = {
+                        severity = h.diagnostics.severities.error,
+                    },
+                },
+            },
+            {
+                pattern = ":(%d+):(%d+): (.*)",
+                groups = { "row", "col", "message" },
+                overrides = {
+                    diagnostic = {
+                        severity = h.diagnostics.severities.warning,
+                    },
+                },
+            },
+        }),
+        dynamic_command = cmd_resolver.from_node_modules(),
+    },
+    factory = h.generator_factory,
+})
