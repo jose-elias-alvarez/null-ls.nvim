1b482af5cde09215d2cdd65825d589f6c0527e3c
Ola Berglund <64544704+olaberglund@users.noreply.github.com>
2022-10-11 00:08:36 +0200

    fix lua related bug caused by dash formatter in name (#1182)
    Co-authored-by: ola weknowit <ola.berglund@weknowit.se>

--- lua/null-ls/builtins/formatting/purs_tidy.lua
+++ lua/null-ls/builtins/formatting/purs_tidy.lua
@@ -4,7 +4,7 @@ local methods = require("null-ls.methods")
 local FORMATTING = methods.internal.FORMATTING
 
 return h.make_builtin({
-    name = "purs-tidy",
+    name = "purs_tidy",
     meta = {
         url = "https://github.com/natefaubion/purescript-tidy",
         description = "A syntax tidy-upper (formatter) for PureScript.",
