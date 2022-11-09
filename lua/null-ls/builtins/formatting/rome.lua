98cb529ee76a46cbcad01e34e5a74a4300c74648
Otto Ahoniemi <otto@ottoahoniemi.fi>
2022-11-09 10:57:49 +0200

    Resolve rome executable from node modules

--- lua/null-ls/builtins/formatting/rome.lua
+++ lua/null-ls/builtins/formatting/rome.lua
@@ -1,4 +1,5 @@
 local h = require("null-ls.helpers")
+local cmd_resolver = require("null-ls.helpers.command_resolver")
 local methods = require("null-ls.methods")
 
 local FORMATTING = methods.internal.FORMATTING
@@ -21,6 +22,7 @@ return h.make_builtin({
             "--write",
             "$FILENAME",
         },
+        dynamic_command = cmd_resolver.from_node_modules(),
         to_stdin = false,
         to_temp_file = true,
     },
