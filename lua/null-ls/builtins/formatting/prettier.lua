7d27c24e6c2058756939d0c97331da74645dabb1
honnip <108175486+honnip@users.noreply.github.com>
2023-01-20 18:30:53 +0900

    docs: fix broken link in prettier section

--- lua/null-ls/builtins/formatting/prettier.lua
+++ lua/null-ls/builtins/formatting/prettier.lua
@@ -12,7 +12,7 @@ return h.make_builtin({
         url = "https://github.com/prettier/prettier",
         description = [[Prettier is an opinionated code formatter. It enforces a consistent style by parsing your code and re-printing it with its own rules that take the maximum line length into account, wrapping code when necessary.]],
         notes = {
-            [[Supports more filetypes such as [Svelte](https://github.com/sveltejs/prettier-plugin-svelte) and [TOML](https://github.com/bd82/toml-tools/tree/master/packages/prettier-plugin-toml) via plugins. These filetypes are not enabled by default, but you can follow the instructions [here](#filetypes) to define your own list of filetypes.]],
+            [[Supports more filetypes such as [Svelte](https://github.com/sveltejs/prettier-plugin-svelte) and [TOML](https://github.com/bd82/toml-tools/tree/master/packages/prettier-plugin-toml) via plugins. These filetypes are not enabled by default, but you can follow the instructions [here](BUILTIN_CONFIG.md#filetypes) to define your own list of filetypes.]],
             [[To increase speed, you may want to try [prettierd](https://github.com/fsouza/prettierd). You can also set up [eslint-plugin-prettier](https://github.com/prettier/eslint-plugin-prettier) and format via [eslint_d](https://github.com/mantoni/eslint_d.js/).]],
         },
     },
