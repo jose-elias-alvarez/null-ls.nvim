0f68a4bcfe7a878d505279df2c31d77093567bdf
Laytan Laats <laytanlaats@hotmail.com>
2023-02-06 10:18:37 +0100

    fix: utils.root_pattern not checking the start_path for patterns

--- lua/null-ls/utils/init.lua
+++ lua/null-ls/utils/init.lua
@@ -254,17 +254,35 @@ M.path = {
 M.root_pattern = function(...)
     local patterns = vim.tbl_flatten({ ... })
 
-    return function(start_path)
-        for path in vim.fs.parents(start_path) do
-            -- escape wildcard characters in the path so that it is not treated like a glob
-            path = path:gsub("([%[%]%?%*])", "\\%1")
-            for _, pattern in ipairs(patterns) do
-                ---@diagnostic disable-next-line: param-type-mismatch
-                for _, p in ipairs(vim.fn.glob(M.path.join(path, pattern), true, true)) do
-                    if M.path.exists(p) then
-                        return path
-                    end
+    local function matcher(path)
+        if not path then
+            return nil
+        end
+
+        -- escape wildcard characters in the path so that it is not treated like a glob
+        path = path:gsub("([%[%]%?%*])", "\\%1")
+        for _, pattern in ipairs(patterns) do
+            ---@diagnostic disable-next-line: param-type-mismatch
+            for _, p in ipairs(vim.fn.glob(M.path.join(path, pattern), true, true)) do
+                if M.path.exists(p) then
+                    return path
                 end
+            end
+        end
+
+        return nil
+    end
+
+    return function(start_path)
+        local start_match = matcher(start_path)
+        if start_match then
+            return start_match
+        end
+
+        for path in vim.fs.parents(start_path) do
+            local match = matcher(path)
+            if match then
+                return match
             end
         end
     end
