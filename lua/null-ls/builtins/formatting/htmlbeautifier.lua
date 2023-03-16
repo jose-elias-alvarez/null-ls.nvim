4444c9740f6aaac8a9f75a68e07c926c3e1238e3
John Pitchko <john.pitchko@icloud.com>
2023-03-16 05:48:29 -0600

    Format w/ stylua

--- lua/null-ls/builtins/formatting/htmlbeautifier.lua
+++ lua/null-ls/builtins/formatting/htmlbeautifier.lua
@@ -4,17 +4,17 @@ local methods = require("null-ls.methods")
 local FORMATTING = methods.internal.FORMATTING
 
 return h.make_builtin({
-  name = "htmlbeautifier",
-  meta = {
-    url = "https://github.com/threedaymonk/htmlbeautifier",
-    description = "A normaliser/beautifier for HTML that also understands embedded Ruby. Ideal for tidying up Rails templates.",
-  },
-  method = FORMATTING,
-  filetypes = { "eruby" },
-  generator_opts = {
-    command = "htmlbeautifier",
-    args = {},
-    to_stdin = true,
-  },
-  factory = h.formatter_factory,
+    name = "htmlbeautifier",
+    meta = {
+        url = "https://github.com/threedaymonk/htmlbeautifier",
+        description = "A normaliser/beautifier for HTML that also understands embedded Ruby. Ideal for tidying up Rails templates.",
+    },
+    method = FORMATTING,
+    filetypes = { "eruby" },
+    generator_opts = {
+        command = "htmlbeautifier",
+        args = {},
+        to_stdin = true,
+    },
+    factory = h.formatter_factory,
 })
