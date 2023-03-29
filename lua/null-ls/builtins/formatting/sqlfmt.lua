c44df5831002005d7a9bf63d61b3a59686227e3a
Ron Morgenstern <ron.morgenstern@pagaya.com>
2023-03-29 13:42:37 +0300

    Add sqlfmt formatter to built-ins

--- lua/null-ls/builtins/formatting/sqlfmt.lua
+++ lua/null-ls/builtins/formatting/sqlfmt.lua
@@ -5,27 +5,27 @@ local u = require("null-ls.utils")
 local FORMATTING = methods.internal.FORMATTING
 
 return h.make_builtin({
-  name = "sqlfmt",
-  meta = {
-    url = "https://sqlfmt.com/",
-    description = "Formats your dbt SQL files so you don't have to",
-    notes = {
-      "Install sqlfmt with `pip install shandy-sqlfmt[jinjafmt]`",
+    name = "sqlfmt",
+    meta = {
+        url = "https://sqlfmt.com/",
+        description = "Formats your dbt SQL files so you don't have to",
+        notes = {
+            "Install sqlfmt with `pip install shandy-sqlfmt[jinjafmt]`",
+        },
     },
-  },
-  METHOD = FORMATTING,
-  filetypes = {
-    "sql",
-    "jinja",
-  },
-  generator_opts = {
-    command = "sqlfmt",
-    args = {},
-    to_stdin = true,
-    check_exit_code = { 0, 1 },
-    cwd = h.cache.by_bufnr(function(params)
-      return u.root_pattern("dbt_project.yml")(params.bufnr) or vim.fn.getcwd()
-    end),
-  },
-  factory = h.formatter_factory,
+    METHOD = FORMATTING,
+    filetypes = {
+        "sql",
+        "jinja",
+    },
+    generator_opts = {
+        command = "sqlfmt",
+        args = {},
+        to_stdin = true,
+        check_exit_code = { 0, 1 },
+        cwd = h.cache.by_bufnr(function(params)
+            return u.root_pattern("dbt_project.yml")(params.bufnr) or vim.fn.getcwd()
+        end),
+    },
+    factory = h.formatter_factory,
 })
