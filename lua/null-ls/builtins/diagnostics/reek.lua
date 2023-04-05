66125bf7c187f517c567d1e31438dd7d0970118e
Benjamin Eskola <ben@eskola.uk>
2023-04-05 08:58:55 +0100

    Add builtin for Reek

--- lua/null-ls/builtins/diagnostics/reek.lua
+++ lua/null-ls/builtins/diagnostics/reek.lua
@@ -1,60 +1,37 @@
 local h = require("null-ls.helpers")
 local methods = require("null-ls.methods")
-
-local DIAGNOSTICS = methods.internal.DIAGNOSTICS
-
-local build_offense = function(record, line)
-    return {
-        message = record.smell_type .. ": " .. record.message,
-        smell_type = record.smell_type,
-        severity = h.diagnostics.severities.warning,
-        row = line,
-        col = 0,
-        end_col = 1,
-        filename = record.source,
-    }
-end
-
-local handle_record = function(record, offenses)
-    for _, line in ipairs(record.lines) do
-        table.insert(offenses, build_offense(record, line))
-    end
-end
-
-local handle_output = function(params)
-    local offenses = {}
-
-    if params.output then
-        for _, record in ipairs(params.output) do
-            handle_record(record, offenses)
-        end
-    end
-
-    return offenses
-end
+local u = require("null-ls.utils")
 
 return h.make_builtin({
     name = "reek",
-
     meta = {
         url = "https://github.com/troessner/reek",
-        description = "Code smell detector for Ruby",
+        description = "Code smell detectory for Ruby",
     },
-
-    method = DIAGNOSTICS,
+    method = methods.internal.DIAGNOSTICS_ON_SAVE,
     filetypes = { "ruby" },
     generator_opts = {
         command = "reek",
-        args = { "--format", "json", "--stdin-filename", "$FILENAME" },
-        to_stdin = true,
-        from_stderr = true,
-        format = "json",
+        args = { "-s", "$FILENAME" },
+        format = "line",
+        multiple_files = true,
+        on_output = function(line, params)
+            local name, row, code, message = line:match("%s+(%g+):(%d+): (%g+): (.+)%s*%f[[]")
+            if not (name and row) then
+                return
+            end
 
-        check_exit_code = function(code)
-            return code <= 1
+            local filename = u.path.join(params.cwd, name)
+
+            local severity = 2 -- warning
+            return {
+                row = row,
+                code = code,
+                message = message,
+                severity = severity,
+                filename = filename,
+            }
         end,
-
-        on_output = handle_output,
     },
     factory = h.generator_factory,
 })
