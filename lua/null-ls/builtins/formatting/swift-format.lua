d8fe1af83a41a8d03170be1f9db95369595a1dc2
Evan Lurvey <54965655+evanlurvey@users.noreply.github.com>
2023-04-21 17:44:29 -0500

    Create swift-format.lua (#1522)

---
+++ lua/null-ls/builtins/formatting/swift-format.lua
@@ -0,0 +1,20 @@
+local h = require("null-ls.helpers")
+local methods = require("null-ls.methods")
+
+local FORMATTING = methods.internal.FORMATTING
+
+return h.make_builtin({
+    name = "swiftformat",
+    meta = {
+        url = "https://github.com/apple/swift-format",
+        description = "Swift formatter from apple. Requires building from source with `swift build`",
+    },
+    method = FORMATTING,
+    filetypes = { "swift" },
+    generator_opts = {
+        command = "swift-format",
+        args = {},
+        to_stdin = true,
+    },
+    factory = h.formatter_factory,
+})
