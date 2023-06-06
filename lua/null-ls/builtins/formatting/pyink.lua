d9e0cbdea3c64ef35eea83d54e1839a401c775a6
kuator <kuatabylkasymov@gmail.com>
2023-06-06 18:48:43 +0600

    feat: add pyink range formatting (#1568)
    * feat: add pyink range formatting
    
    * fix braces

--- lua/null-ls/builtins/formatting/pyink.lua
+++ lua/null-ls/builtins/formatting/pyink.lua
@@ -2,6 +2,7 @@ local h = require("null-ls.helpers")
 local methods = require("null-ls.methods")
 
 local FORMATTING = methods.internal.FORMATTING
+local RANGE_FORMATTING = methods.internal.RANGE_FORMATTING
 
 return h.make_builtin({
     name = "pyink",
@@ -9,16 +10,16 @@ return h.make_builtin({
         url = "https://github.com/google/pyink",
         description = "The Google Python code formatter",
     },
-    method = FORMATTING,
+    method = { FORMATTING, RANGE_FORMATTING },
     filetypes = { "python" },
     generator_opts = {
         command = "pyink",
-        args = {
+        args = h.range_formatting_args_factory({
             "--stdin-filename",
             "$FILENAME",
             "--quiet",
             "-",
-        },
+        }, "--pyink-lines", nil, { use_rows = true, delimiter = "-" }),
         to_stdin = true,
     },
     factory = h.formatter_factory,
