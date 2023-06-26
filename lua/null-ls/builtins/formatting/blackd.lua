25b8b6efbce3e658ab951f5f5b5868f1af32272f
tiagovla <tiagovla@gmail.com>
2023-06-26 14:12:34 -0300

    feat: add blackd

---
+++ lua/null-ls/builtins/formatting/blackd.lua
@@ -0,0 +1,34 @@
+local h = require("null-ls.helpers")
+local methods = require("null-ls.methods")
+local curl = require("plenary.curl")
+
+local FORMATTING = methods.internal.FORMATTING
+
+return h.make_builtin({
+    name = "blackd",
+    meta = {
+        url = "https://github.com/psf/black",
+        description = "The uncompromising Python code formatter",
+    },
+    method = FORMATTING,
+    filetypes = { "python" },
+    generator = {
+        fn = function(params)
+            local hostname = params.options.hostname or "localhost"
+            local port = params.options.port or 45484
+            local response = curl.post(hostname .. ":" .. port, {
+                body = table.concat(params.content, "\n"),
+                headers = {
+                    ["X-Line-Length"] = params.options.line_length,
+                    ["X-Skip-Source-First-Line"] = params.options.skip_source_first_line and "true",
+                    ["X-Skip-String-Normalization"] = params.options.skip_string_normalization and "true",
+                    ["X-Skip-Magic-Trailing-Comma"] = params.options.skip_magic_trailing_comma and "true",
+                    ["X-Preview"] = params.options.preview and "true",
+                    ["Fast-Or-Safe"] = params.options.fast_or_safe and "fast",
+                    ["Python-Variant"] = params.options.python_variant,
+                },
+            })
+            return response.body == "" and {} or { { text = response.body } }
+        end,
+    },
+})
