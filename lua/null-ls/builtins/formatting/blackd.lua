63e8631e6cf40f839d1d1c4d7c5727636a2732cc
tiagovla <tiagovla@gmail.com>
2023-06-29 23:52:20 -0300

    fix: use generator key instead of factory key

--- lua/null-ls/builtins/formatting/blackd.lua
+++ lua/null-ls/builtins/formatting/blackd.lua
@@ -13,32 +13,30 @@ return h.make_builtin({
     },
     method = FORMATTING,
     filetypes = { "python" },
-    factory = function()
-        return {
-            async = true,
-            fn = function(params, done)
-                local config = params:get_config()
-                local hostname = config.hostname or "localhost"
-                local port = config.port or 45484
-                curl.post(hostname .. ":" .. port, {
-                    body = table.concat(params.content, "\n"),
-                    headers = {
-                        ["X-Line-Length"] = config.line_length,
-                        ["X-Skip-Source-First-Line"] = config.skip_source_first_line,
-                        ["X-Skip-String-Normalization"] = config.skip_string_normalization,
-                        ["X-Skip-Magic-Trailing-Comma"] = config.skip_magic_trailing_comma,
-                        ["X-Preview"] = config.preview,
-                        ["X-Fast-Or-Safe"] = config.fast_or_safe,
-                        ["X-Python-Variant"] = config.python_variant,
-                    },
-                    callback = function(response)
-                        if response.status == 200 then
-                            return done({ { text = response.body } })
-                        end
-                        return done()
-                    end,
-                })
-            end,
-        }
-    end,
+    generator = {
+        async = true,
+        fn = function(params, done)
+            local config = params:get_config()
+            local hostname = config.hostname or "localhost"
+            local port = config.port or 45484
+            curl.post(hostname .. ":" .. port, {
+                body = table.concat(params.content, "\n"),
+                headers = {
+                    ["X-Line-Length"] = config.line_length,
+                    ["X-Skip-Source-First-Line"] = config.skip_source_first_line,
+                    ["X-Skip-String-Normalization"] = config.skip_string_normalization,
+                    ["X-Skip-Magic-Trailing-Comma"] = config.skip_magic_trailing_comma,
+                    ["X-Preview"] = config.preview,
+                    ["X-Fast-Or-Safe"] = config.fast_or_safe,
+                    ["X-Python-Variant"] = config.python_variant,
+                },
+                callback = function(response)
+                    if response.status == 200 then
+                        return done({ { text = response.body } })
+                    end
+                    return done()
+                end,
+            })
+        end,
+    },
 })
