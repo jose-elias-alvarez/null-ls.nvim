ad62632d23db499b46e5d64ab98a54d808bd0b62
tiagovla <tiagovla@gmail.com>
2023-06-29 23:51:12 -0300

    fix: user config instead of args

--- lua/null-ls/builtins/formatting/blackd.lua
+++ lua/null-ls/builtins/formatting/blackd.lua
@@ -13,23 +13,23 @@ return h.make_builtin({
     },
     method = FORMATTING,
     filetypes = { "python" },
-    factory = function(opts)
+    factory = function()
         return {
             async = true,
             fn = function(params, done)
-                local options = u.handle_function_opt(opts.args)
-                local hostname = options.hostname or "localhost"
-                local port = options.port or 45484
+                local config = params:get_config()
+                local hostname = config.hostname or "localhost"
+                local port = config.port or 45484
                 curl.post(hostname .. ":" .. port, {
                     body = table.concat(params.content, "\n"),
                     headers = {
-                        ["X-Line-Length"] = options.line_length,
-                        ["X-Skip-Source-First-Line"] = options.skip_source_first_line,
-                        ["X-Skip-String-Normalization"] = options.skip_string_normalization,
-                        ["X-Skip-Magic-Trailing-Comma"] = options.skip_magic_trailing_comma,
-                        ["X-Preview"] = options.preview,
-                        ["X-Fast-Or-Safe"] = options.fast_or_safe,
-                        ["X-Python-Variant"] = options.python_variant,
+                        ["X-Line-Length"] = config.line_length,
+                        ["X-Skip-Source-First-Line"] = config.skip_source_first_line,
+                        ["X-Skip-String-Normalization"] = config.skip_string_normalization,
+                        ["X-Skip-Magic-Trailing-Comma"] = config.skip_magic_trailing_comma,
+                        ["X-Preview"] = config.preview,
+                        ["X-Fast-Or-Safe"] = config.fast_or_safe,
+                        ["X-Python-Variant"] = config.python_variant,
                     },
                     callback = function(response)
                         if response.status == 200 then
