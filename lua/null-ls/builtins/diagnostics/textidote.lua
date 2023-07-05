de2562b7f1b4eb8e809ec88d883ed144a61501e7
tiagovla <tiagovla@gmail.com>
2023-07-04 21:33:16 -0300

    feat: add textidote support

---
+++ lua/null-ls/builtins/diagnostics/textidote.lua
@@ -0,0 +1,41 @@
+local h = require("null-ls.helpers")
+local methods = require("null-ls.methods")
+
+local DIAGNOSTICS = methods.internal.DIAGNOSTICS
+
+return h.make_builtin({
+    name = "textidote",
+    meta = {
+        url = "https://github.com/sylvainhalle/textidote",
+        description = "Spelling, grammar and style checking on LaTeX documents.",
+    },
+    method = DIAGNOSTICS,
+    filetypes = { "markdown", "tex" },
+    generator_opts = {
+        command = "textidote",
+        args = {
+            "--read-all",
+            "--output",
+            "singleline",
+            "--no-color",
+            "--check",
+            "en",
+            "$FILENAME",
+        },
+        format = "line",
+        ignore_stderr = true,
+        to_stdin = true,
+        on_output = h.diagnostics.from_patterns({
+            {
+                pattern = [[(.*)%(L(%d+)C(%d+)-L(%d+)C(%d+)%): (.*)]],
+                groups = { "filename", "row", "col", "end_row", "end_col", "message" },
+                overrides = {
+                    diagnostic = {
+                        severity = h.diagnostics.severities.warning,
+                    },
+                },
+            },
+        }),
+    },
+    factory = h.generator_factory,
+})
